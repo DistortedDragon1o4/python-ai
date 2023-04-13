710b9531e94ab03f5f7744c479156a530734c2fd
Shameek Dalal <99723753+DistortedDragon1o4@users.noreply.github.com>
2023-04-13 20:35:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,11 @@ headers = {"Authorization": "Bearer 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"}
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-# outputStr = "<|prompter|>tell me about India's history<|endoftext|><|assistant|>"
-# output = []
-
-# i = 0
-# while i < 10:
-#     output = query({"inputs": outputStr, "parameters": {"temperature": 1.11}})
-#     outputStr = output[0]["generated_text"]
-#     i += 1
-
-# outputStr = outputStr + "<|endoftext|><|prompter|>tell me about yourself<|endoftext|><|assistant|>"
-
-# i = 0
-# while i < 10:
-#     output = query({"inputs": outputStr})
-#     outputStr = output[0]["generated_text"]
-#     i += 1
-
-# output = query({
-# 	"inputs": "<|prompter|>What is a meme, and what\'s the history behind this word?<|endoftext|><|assistant|>The word \"meme\" was first used in the 1960s by Richard Dawkins to describe an idea, behavior, or style that is transmitted from one individual to another within a culture. The concept",
-# })
-
-# outputStr = output[0]["generated_text"]
 
 def promptGen():
     crntPrompt = input("Please ask your question: ")
+    if crntPrompt.lower() == "exit":
+        return "exit"
     crntPrompt = "<|prompter|>" + crntPrompt + "<|endoftext|><|assistant|>"
     return crntPrompt
 
@@ -41,7 +20,7 @@ def promptQuery(prompt):
     output = []
     i = 0
     while i < 10:
-        output = query({"inputs": outputStr, "parameters": {"temperature": 1.11}})
+        output = query({"inputs": outputStr, "parameters": {"temperature": 1.0}})
         try:
             outputStr = output[0]["generated_text"]
         except:
@@ -56,10 +35,23 @@ def fetchReply(output):
     i2 = output.rfind("<|endoftext|>")
     return output[i1 + 13:i2]
 
-prompt = promptGen()
-output = promptQuery(prompt)
-print(fetchReply(output))
+def deleteLast(output):
+    i = output.find("<|endoftext|>")
+    output = output[i + 13:]
+    i = output.find("<|endoftext|>")
+    return output[i + 13:]
 
+def numPrompts(output):
+    return output.count("<|assistant|>")
 
+allPrompts = ""
 
-# print(outputStr)
+while True:
+    prompt = promptGen()
+    if prompt == "exit":
+        break
+    output = promptQuery(allPrompts + prompt)
+    print(fetchReply(output))
+    allPrompts = output
+    if numPrompts(allPrompts) > 5:
+        deleteLast(allPrompts)
